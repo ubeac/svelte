c428df359f20f985cf4034d4eab4b70b02bfae65
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-04-28 23:52:18 +0430

    update storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,22 +1,10 @@
-const { mergeConfig } = require('vite');
-const preprocess = require('svelte-preprocess')
-const path = require('path')
+const preprocess = require('svelte-preprocess');
 
-console.log(preprocess)
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
 	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-svelte-csf'],
 	framework: '@storybook/svelte',
 	svelteOptions: {
 		preprocess: preprocess()
-	},
-	async viteFinal(config, {configType}) {
-		return mergeConfig(config, {
-			resolve: {
-				alias: {
-					'$lib': path.resolve('./src/lib')
-				}
-			}
-		})
 	}
 };
