950632806b9370546e170b7caf0382d80f3b7a3b
Masood Abdolian <m.abdolian@gmail.com>
2022-05-01 03:48:34 +0430

    add icon

---
+++ src/components/icon/Icon.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte'
+	import clsx from 'clsx'
+
+	import type { Colors } from '../../types'
+
+	/**
+	 * TODO
+	 */
+	let klass: string | undefined = undefined
+	export { klass as class }
+
+	/**
+	 * TODO
+	 */
+	export let color: Colors = undefined
+
+	/**
+	 * TODO
+	 */
+	export let name: string | undefined = undefined
+
+	/**
+	 * TODO
+	 */
+	export let pack: 'tabler' = 'tabler'
+
+	/**
+	 * TODO
+	 */
+	export let size:
+		| 'auto'
+		| 'xs'
+		| 'sm'
+		| 'md'
+		| 'lg'
+		| 'xl'
+		| '1x'
+		| '2x'
+		| '3x'
+		| '4x'
+		| '5x'
+		| '6x'
+		| '7x'
+		| '8x'
+		| '9x' = 'auto'
+
+	$: classes = clsx(
+		'u-icon',
+		{
+			[`u-icon-${size}`]: size,
+			[`u-text-${color}`]: color,
+		},
+		klass
+	)
+</script>
+
+<Icon class={classes} icon="{pack}:{name}" {...$$restProps} />
+
+<!-- TODO -->
+<style lang="scss" global>
+	@import '../../scss';
+</style>
---
+++ src/stories/icon/Icon.stories.svelte
@@ -0,0 +1,47 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+
+	import { COLORS } from '../../types'
+	import { Icon } from '../../components'
+</script>
+
+<Meta
+	title="Icon"
+	component={Icon}
+	argTypes={{
+		color: {
+			control: {
+				type: 'select',
+				options: COLORS,
+			},
+		},
+		size: {
+			defaultValue: 'auto',
+			control: {
+				type: 'select',
+				options: [
+					'auto',
+					'xs',
+					'sm',
+					'md',
+					'lg',
+					'xl',
+					'1x',
+					'2x',
+					'3x',
+					'4x',
+					'5x',
+					'6x',
+					'7x',
+					'8x',
+					'9x',
+				],
+			},
+		},
+	}} />
+
+<Template let:args>
+	<Icon {...args} />
+</Template>
+
+<Story name="default" args={{ name: 'menu' }} />
