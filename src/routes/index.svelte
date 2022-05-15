72119d066bea218537a0dde7ca7c41fc46819bec
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-05-15 22:20:07 +0430

    update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import { Button } from '@app/components'
 
-	// import '../scss/index.scss'
+	// DEBUG=vite:import-analysis npm run dev >> takes about 18 seconds for below import
+	import '../scss/index.scss'
 </script>
 
 <Button>Button</Button>
