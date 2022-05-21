a17dfeadc974319bd9704a0f6a09b3375ecb59ba
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-05-22 00:03:28 +0430

    add vertical and size props

--- src/components/button/ButtonGroup.svelte
+++ src/components/button/ButtonGroup.svelte
@@ -2,8 +2,23 @@
 	import { get_current_component } from 'svelte/internal'
 
 	import { Root } from '@app/components'
+
+	/**
+	 * Align Buttons vertically
+	 */
+	export let vertical: boolean = false
+
+	/**
+	 * Size of buttons
+	 */
+	export let size: 'sm' | 'md' | 'lg' = 'md'
+
+	$: classes = {
+		vertical,
+		size,
+	}
 </script>
 
-<Root element="div" component={get_current_component()} componentName="ButtonGroup" {...$$restProps}>
+<Root element="div" {classes} component={get_current_component()} componentName="ButtonGroup" {...$$restProps}>
 	<slot />
 </Root>
