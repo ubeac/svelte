b76e2e414745250bea856c019ff2e772b9831053
Masood Abdolian <m.abdolian@gmail.com>
2022-05-24 22:10:21 +0430

    init badge

---
+++ src/components/badge/Badge.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import { get_current_component } from 'svelte/internal'
+
+	import { Root } from '@app/components'
+	import type { Colors, PlacementEdge } from '@app/types'
+
+	/**
+	 * TODO
+	 */
+	export let color: Colors = undefined
+
+	/**
+	 * TODO
+	 */
+	export let dot: boolean = false
+
+	/**
+	 * TODO
+	 */
+	export let href: string | undefined = undefined
+
+	/**
+	 * TODO
+	 */
+	export let outline: boolean = false
+
+	/**
+	 * TODO
+	 */
+	export let overlap: boolean = false
+
+	/**
+	 * TODO
+	 */
+	export let placement: PlacementEdge = undefined
+
+	/**
+	 * TODO
+	 */
+	export let round: boolean = false
+
+	/**
+	 * TODO
+	 */
+	export let soft: boolean = false
+
+	/**
+	 * TODO
+	 */
+	export let tile: boolean = false
+
+	$: classes = {
+		color,
+		dot,
+		href,
+		outline,
+		overlap,
+		placement,
+		round,
+		soft,
+		tile,
+	}
+</script>
+
+<Root element="div" {classes} component={get_current_component()} componentName="Badge" {...$$restProps}>
+	<slot />
+</Root>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export * from './badge'
 export * from './button'
 export * from './divider'
 export * from './grid'
