b905572bdbfefeca83ea4976ca61f27b2cdb81f0
Masood Abdolian <m.abdolian@gmail.com>
2022-05-30 19:20:41 +0430

    init avatar

---
+++ src/components/avatar/Avatar.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { get_current_component } from 'svelte/internal'
+
+	import { Icon, Root } from '@app/components'
+	import type { Colors } from '@app/types'
+
+	/**
+	 * Set color of Badge
+	 */
+	export let color: Colors = undefined
+
+	/**
+	 * TODO
+	 */
+	export let shape: 'circle' | 'round' | 'square' = 'round'
+
+	/**
+	 * TODO
+	 */
+	export let size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' = 'md'
+
+	$: classes = {
+		color,
+		shape,
+		size,
+		// TODO
+		// [`bg-${color}-lt`]: !!color,
+		// [`avatar-${size}`]: !!size,
+		// 'rounded': shape === 'rounded',
+		// 'rounded-circle': shape === 'circle',
+		// 'rounded-0': shape === 'square',
+	}
+</script>
+
+<!-- TODO -->
+<!-- style="background-image: url({src})" -->
+<Root element="span" {classes} component={get_current_component()} componentName="Avatar" {...$$restProps}>
+	<slot />
+</Root>
