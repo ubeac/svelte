fdae63f69d72d3227ad00865f3aa2b429cae5bde
Masood Abdolian <m.abdolian@gmail.com>
2022-05-31 21:28:36 +0430

    update label

--- src/components/label/Label.svelte
+++ src/components/label/Label.svelte
@@ -2,14 +2,8 @@
 	import { get_current_component } from 'svelte/internal'
 
 	import { Root } from '../root'
-
-	/**
-	 * id of a signle form-related element in document
-	 */
-	let fore: string | undefined = undefined
-	export { fore as for }
 </script>
 
-<Root element="label" componentName="Label" component={get_current_component()} for={fore}>
+<Root element="label" component={get_current_component()} componentName="Label" {...$$restProps}>
 	<slot />
 </Root>
