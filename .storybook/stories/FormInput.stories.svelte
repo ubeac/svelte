c92ca677e89447f316f4979a08c8406f5f8ef7d0
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-06-11 12:05:59 +0430

    add stories for FormInput and Input components

---
+++ .storybook/stories/FormInput.stories.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+	import { FormInput } from '@app/components'
+</script>
+
+<Meta component={FormInput} title="FormInput" />
+
+<Template let:args>
+	<FormInput {...args} />
+</Template>
+
+<Story name="default" />
+
+<Story name="disabled" args={{ disabled: true, value: 'Content' }} />
+<Story name="id" args={{ id: 'input-1' }} />
+<Story name="icon" args={{ value: 'content', icon: 'user' }} />
+<Story name="loading" args={{ loading: true }} />
+<Story name="icon + loading" args={{ icon: 'package', loading: true }} />
+<Story name="placeholder" args={{ placeholder: 'this is placeholder' }} />
+<Story name="preview" args={{ value: 'content', preview: true }} />
+<Story name="read-only" args={{ value: 'content', readonly: true }} />
+<Story name="type" args={{ type: 'password', value: 'Test' }} />
+<Story name="type: number" args={{ type: 'number', value: '123' }} />
+<Story name="value" args={{ value: 'Test' }} />
---
+++ .storybook/stories/Input.stories.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+	import { Input } from '@app/components'
+</script>
+
+<Meta component={Input} title="Input" />
+
+<Template let:args>
+	<Input {...args} />
+</Template>
+
+<Story name="default" />
+
+<Story name="preview" args={{ value: 'content', preview: true }} />
+<Story name="disabled" args={{ disabled: true, value: 'Content' }} />
+<Story name="read-only" args={{ value: 'content', readonly: true }} />
+<Story name="placeholder" args={{ placeholder: 'this is placeholder' }} />
+<Story name="value" args={{ value: 'Test' }} />
+<Story name="type" args={{ type: 'password', value: 'Test' }} />
+<Story name="id" args={{ id: 'input-1' }} />
