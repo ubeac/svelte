71c5c022dd30977b440ccee2882d5fd703199181
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-06-13 22:34:33 +0430

    add stories and docs for AvatarGroup component

---
+++ .storybook/stories/AvatarGroup.stories.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+	import { Avatar } from '@app/components'
+</script>
+
+<Meta id="Avatar Group" component={Avatar.Group} />
+
+<Template let:args>
+	<Avatar.Group {...args}>
+		<Avatar>A</Avatar>
+		<Avatar>B</Avatar>
+		<Avatar>C</Avatar>
+		<Avatar>D</Avatar>
+	</Avatar.Group>
+	<br />
+	<br />
+	<br />
+	<Avatar.Group {...args}>
+		<Avatar size="sm" shape="circle">A</Avatar>
+		<Avatar size="sm" shape="circle">B</Avatar>
+		<Avatar size="sm" shape="circle">C</Avatar>
+		<Avatar size="sm" shape="circle">D</Avatar>
+	</Avatar.Group>
+</Template>
+
+<Story name="default" />
+<Story name="stacked" args={{ stacked: true }} />
