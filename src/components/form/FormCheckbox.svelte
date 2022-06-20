e42b39f3610a5576eff57779cf1194956da97d41
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-06-20 12:24:25 +0430

    add id prop for FormCheckbox

--- src/components/form/FormCheckbox.svelte
+++ src/components/form/FormCheckbox.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
 	import { get_current_component } from 'svelte/internal'
 
+	import { nanoid } from 'nanoid'
+
 	import { forwardEventsBuilder } from '$lib/directives'
 	import { classname, condition } from '$lib/utils'
 
 	import { Checkbox } from '../input'
+
+	/**
+	 * TODO
+	 */
+	export let id = nanoid(10)
 
 	/**
 	 * Aligns multiple elements in same line
@@ -32,9 +39,8 @@
 </script>
 
 {#if condition($$props)}
-	<!-- svelte-ignore a11y-label-has-associated-control -->
-	<label use:forwardEvents {required} {...$$restProps} class={classes}>
-		<Checkbox on:change bind:value {required} {...$$restProps} />
+	<label for={id} use:forwardEvents {required} {...$$restProps} class={classes}>
+		<Checkbox {id} on:change bind:value {required} {...$$restProps} />
 		{#if label}
 			<span class={classname('form-checkbox-label')}>{label}</span>
 		{/if}
