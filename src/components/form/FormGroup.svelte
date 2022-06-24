d404792295c719020e75b4c469687284d270335c
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-06-24 15:21:05 +0430

    remove storybook errors

--- src/components/form/FormGroup.svelte
+++ src/components/form/FormGroup.svelte
@@ -3,10 +3,12 @@
 	import { classname, condition } from '$lib/utils'
 
 	$: classes = classname('form-group', null, $$props.class)
+
+	$: col = $$props.col ?? '6'
 </script>
 
 {#if condition($$props)}
-	<Grid.Item {...$$restProps} class={classes} col={$$props.col ?? '6'}>
+	<Grid.Item {...$$restProps} class={classes} {col}>
 		<div>
 			<slot />
 		</div>
