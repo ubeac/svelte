da0f0c43a814dfc46d5dd14b85f184975c6dd164
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-06-24 14:25:12 +0430

    remove form elements

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -1,4 +1,3 @@
-export { default as FormCheckbox } from './FormCheckbox.svelte'
 export { default as FormFieldset } from './FormFieldset.svelte'
 export { default as FormGroup } from './FormGroup.svelte'
 export { default as FormInput } from './FormInput.svelte'
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Checkbox, FormCheckbox } from '$lib/components'
+	import { Checkbox } from '$lib/components'
 
 	let check1Value: boolean = false
 	let check2Value: boolean = false
@@ -11,11 +11,6 @@
 <Checkbox bind:value={check1Value} disabled />
 <Checkbox bind:value={check2Value} indeterminate />
 <Checkbox bind:value={check1Value} indeterminate />
-
-<br />
-
-<FormCheckbox label="check 1" bind:value={check1Value} />
-<FormCheckbox label="check 2" bind:value={check2Value} />
 
 <style lang="scss" global>
 	@import '../scss';
