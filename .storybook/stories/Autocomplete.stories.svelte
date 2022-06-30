800606c4e66d7b0f9c8fcf8de78cd3ba293b4cdf
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-06-30 15:07:40 +0430

    adding story for Autocomplete component

---
+++ .storybook/stories/Autocomplete.stories.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+	import { Autocomplete } from '$lib/components'
+
+	let items1 = [1, 2, 3, 10, 11, 12, 13, 21, 22, 23, 31, 32, 33]
+	let items2 = ['a', 'b', 'c', 'aa', 'ab', 'ac', 'ad', 'ba', 'bb', 'bc', 'ca', 'cb', 'cc']
+</script>
+
+<Meta title="Autocomplete" component={Autocomplete} />
+
+<Template let:args>
+	<Autocomplete {...args} />
+</Template>
+
+<Story name="default" />
+
+<Story name="placeholder" args={{ items: items1, placeholder: 'this is placeholder' }} />
+<Story name="placeholder" args={{ items: items2, placeholder: 'this is placeholder' }} />
+<Story name="preview" args={{ items: items1, placeholder: 'this is placeholder', preview: true }} />
+<Story name="preview" args={{ items: items2, placeholder: 'this is placeholder', preview: true }} />
+<Story name="readonly" args={{ items: items1, placeholder: 'this is placeholder', readonly: true }} />
+<Story name="readonly" args={{ items: items2, placeholder: 'this is placeholder', readonly: true }} />
+<Story name="disabled" args={{ items: items1, placeholder: 'this is placeholder', disabled: true }} />
+<Story name="disabled" args={{ items: items2, placeholder: 'this is placeholder', disabled: true }} />
+<Story name="multiple" args={{ items: items1, placeholder: 'this is placeholder', multiple: true }} />
+<Story name="multiple" args={{ items: items2, placeholder: 'this is placeholder', multiple: true }} />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,35 +1,19 @@
 <script lang="ts">
-	import { Checkbox, Radio } from '$lib/components'
+	import { Checkbox, FormCheckbox, Radio } from '$lib/components'
+	import { Autocomplete } from '$lib/components'
 
-	let model1 = undefined
-	let gender = 'F'
+	let items1 = [1, 2, 3, 10, 11, 12, 13, 21, 22, 23, 31, 32, 33]
+	let items2 = ['a', 'b', 'c', 'aa', 'ab', 'ac', 'ad', 'ba', 'bb', 'bc', 'ca', 'cb', 'cc']
 
-	let check1Value: boolean = false
-	let check2Value: boolean = false
-
-	setTimeout(() => {
-		gender = 'M'
-	}, 3000)
-
-	function onChange(event) {
-		console.log(event.target.value, event.target.checked)
-	}
+	let disabled = false
+	let multiple = false
 </script>
 
-<Radio value="value1" bind:group={model1} on:change={onChange} />
-{model1}
+disabled
+<Checkbox bind:value={disabled} />
+multiple
+<Checkbox bind:value={multiple} />
+{disabled} - {multiple}
 
-<br />
-
-<Radio name="gender" value="M" bind:group={gender} on:change={onChange} />
-<Radio name="gender" value="F" bind:group={gender} on:change={onChange} />
-{gender}
-
-<br />
-
-<Checkbox bind:value={check1Value} on:change={onChange} /> - {check1Value}
-<Checkbox bind:value={check2Value} on:change={onChange} /> - {check2Value}
-<Checkbox bind:value={check2Value} on:change={onChange} disabled /> - {check2Value}
-<Checkbox bind:value={check1Value} on:change={onChange} disabled /> - {check1Value}
-<Checkbox bind:value={check2Value} on:change={onChange} indeterminate /> - {check2Value}
-<Checkbox bind:value={check1Value} on:change={onChange} indeterminate /> - {check1Value}
+<Autocomplete {disabled} {multiple} items={items1} />
+<Autocomplete {disabled} {multiple} items={items2} />
