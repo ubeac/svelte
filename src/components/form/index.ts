6453377ae249514989b476979b8fd5588c00e234
Masood Abdolian <m.abdolian@gmail.com>
2022-07-05 18:06:54 +0430

    add FormDatePicker

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -1,5 +1,6 @@
 export { default as FormAutocomplete } from './FormAutocomplete.svelte'
 export { default as FormCheckbox } from './FormCheckbox.svelte'
+export { default as FormDatePicker } from './FormDatePicker.svelte'
 export { default as FormFieldset } from './FormFieldset.svelte'
 export { default as FormGroup } from './FormGroup.svelte'
 export { default as FormInput } from './FormInput.svelte'
