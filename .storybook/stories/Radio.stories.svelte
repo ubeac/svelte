86c264242f1fb19174c4a91590f68f3e9dd93bd8
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-07-08 15:23:38 +0430

    add stories for Radio component

---
+++ .storybook/stories/Radio.stories.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+	import { Radio } from '$lib/components'
+</script>
+
+<Meta
+	title="Radio"
+	component={Radio}
+	argTypes={{
+		group: {
+			control: 'text',
+		},
+	}} />
+
+<Template let:args>
+	<Radio name="radio" value="radio1" {...args} />
+	<Radio name="radio" value="radio2" {...args} />
+	<Radio name="radio" value="radio3" {...args} />
+</Template>
+
+<Story name="default" />
+<Story name="group" args={{ group: 'radio2' }} />
