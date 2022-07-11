4bac78848d5f8ad71ebcb6af26f18513108af654
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-07-11 22:21:31 +0430

    add AppLoadingBar component

---
+++ src/components/app-loading/AppLoadingBar.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte'
+	import { tweened } from 'svelte/motion'
+
+	import type { Colors } from '$lib/types'
+	import { classname } from '$lib/utils'
+
+	const dispatch = createEventDispatcher()
+
+	/**
+	 * The Color of loading bar
+	 */
+	export let color: Colors = 'primary'
+
+	/**
+	 * Use Indeterminate prop when you don't know how long the loading will take
+	 */
+	export let indeterminate: boolean = false
+
+	/**
+	 * The amount of time that loading will take
+	 */
+	export let duration: number = 1000
+
+	/**
+	 * Show and start the loading when component mounted
+	 */
+	export let show: boolean = false
+
+	let initialMax: number = 100
+	let value = tweened<number>(0, { duration })
+	let tasksLength: number = 0
+	let intervalId: any
+
+	function increment(val: number) {
+		$value += val
+	}
+
+	export function start() {
+		show = true
+		dispatch('start')
+		value.set(0, { duration: 0 })
+		intervalId = setInterval(() => increment((initialMax - $value) / 4), duration)
+	}
+
+	export function push(number = initialMax / 4) {
+		show = true
+		dispatch('push', number)
+		tasksLength += number
+	}
+
+	export function pop(number = initialMax / 4) {
+		dispatch('pop', number)
+		tasksLength -= number
+		if (tasksLength <= 0) {
+			tasksLength = 0
+			done()
+		}
+	}
+
+	export function done() {
+		dispatch('done')
+		clearInterval(intervalId)
+		value.set(max, { duration: duration / 2 })
+		setTimeout(() => (show = false), duration)
+	}
+
+	$: if ($value > (max * 95) / 100) {
+		clearInterval(intervalId)
+	}
+
+	$: if (show) start()
+
+	$: max = initialMax + tasksLength
+
+	$: classes = classname(
+		'app-loading',
+		{
+			color,
+			indeterminate,
+			show,
+		},
+		$$props.class
+	)
+</script>
+
+<div class={classes}>
+	<div style="width: {($value * 100) / max}%;" class={classname('app-loading-bar')} />
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+	import type { SvelteComponent } from 'svelte'
+
+	import { AppLoadingBar, Button, Card, CardBody, CardHeader, FormCheckbox, Select } from '$lib/components'
+	import type { Colors } from '$lib/types'
+
+	let el: SvelteComponent
+	let color: Colors
+	let indeterminate: boolean
+	let showInCard: boolean = false
+
+	onMount(() => {
+		el.start()
+	})
+
+	function start() {
+		color = 'primary'
+		el.start()
+	}
+
+	function done() {
+		el.done()
+	}
+
+	function push() {
+		el.push()
+	}
+
+	function pop() {
+		el.pop()
+	}
+
+	function success() {
+		color = 'success'
+		el.done()
+	}
+	function fail() {
+		color = 'danger'
+		el.done()
+	}
+</script>
+
+<AppLoadingBar {color} {indeterminate} bind:this={el} />
+
+<Card outline class="m-4">
+	{#if showInCard}
+		<AppLoadingBar {color} {indeterminate} />
+	{/if}
+	<CardHeader>Controls</CardHeader>
+	<CardBody>
+		<FormCheckbox bind:value={indeterminate} label="Indeterminate" />
+		<FormCheckbox bind:value={showInCard} label="Show inside card?" />
+		<Select
+			bind:value={color}
+			items={['primary', 'secondary', 'success', 'danger', 'red', 'blue', 'warning', 'gray', 'purple']} />
+		<Button on:click={start}>Start</Button>
+		<Button on:click={done}>Done</Button>
+		<Button on:click={fail}>Fail</Button>
+		<Button on:click={success}>Success</Button>
+		<Button on:click={push}>Push</Button>
+		<Button on:click={pop}>Pop</Button>
+	</CardBody>
+</Card>
