338cf3b673e8135406570e8376fe793450bcb5e5
Masood Abdolian <m.abdolian@gmail.com>
2022-07-14 22:42:37 +0430

    add switch

---
+++ src/components/input/Switch.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { get_current_component } from 'svelte/internal'
+
+	import { forwardEventsBuilder } from '$lib/directives'
+	import { classname, condition } from '$lib/utils'
+
+	/**
+	 * Forward all native Events
+	 */
+	export let forwardEvents = forwardEventsBuilder(get_current_component())
+
+	/**
+	 * Checked state of Checkbox
+	 */
+	export let value: boolean | undefined = undefined
+
+	$: classes = classname('switch', null, $$props.class)
+</script>
+
+{#if condition($$props)}
+	<label class={classes}>
+		<input bind:checked={value} use:forwardEvents {...$$restProps} type="checkbox" />
+	</label>
+{/if}
