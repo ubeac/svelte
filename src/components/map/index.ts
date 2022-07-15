c17978c6c9a570d8287c513ba65c33cf53c234cc
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-07-15 16:35:53 +0430

    add GoogleAutocomplete component

--- src/components/map/index.ts
+++ src/components/map/index.ts
@@ -1,1 +1,2 @@
+export {default as GoogleAutocomplete} from './GoogleAutocomplete.svelte'
 export {default as GoogleMap} from './GoogleMap.svelte'
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
-    import {Button, GoogleMap} from '$lib/components'
-    import type { Coordinate } from '$lib/types';
+	import { Button, GoogleAutocomplete, GoogleMap } from '$lib/components'
+	import type { Coordinate } from '$lib/types'
 
+	let autocompleteValue: any
+	let value: Coordinate
 
-    let value: Coordinate;
+	// $: {
+	// 	value = {
+	// 		lat: autocompleteValue?.address_components?.[0].geometry?.location.lat,
+	// 		lng: autocompleteValue?.address_components?.[0].geometry?.location.lng,
+	// 	}
+	// }
 </script>
 
 <Button>Some Button</Button>
-<pre>{JSON.stringify(value, null, 2)}
-</pre>
-<GoogleMap bind:value/>
+<GoogleAutocomplete bind:value={autocompleteValue} />
+<pre>AUTOCOMPLETE: {JSON.stringify(autocompleteValue, null, 2)}</pre>
+
+<br /><br />
+<GoogleMap bind:value />
+<pre>MAP: {JSON.stringify(value, null, 2)}</pre>
