8988acb1bd79ded0ebd669a68931be38b6d2158e
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-07-18 11:30:54 +0430

    add missing stories

--- .storybook/stories/GoogleMap.stories.svelte
+++ .storybook/stories/GoogleMap.stories.svelte
@@ -2,13 +2,9 @@
 	import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
 	import { GoogleMap } from '$lib/components'
 
-	let apiKey = 'AIzaSyBJKPIYzpZOPKyhpgvPCKeWPEAoaiQBHJ8'
-</script>
+	let api_Key = 'AIzaSyBJKPIYzpZOPKyhpgvPCKeWPEAoaiQBHJ8'
 
-<Meta
-	title="GoogleMap"
-	component={GoogleMap}
-	argTypes={{
+	let argTypes = {
 		apiKey: {
 			control: 'text',
 		},
@@ -18,10 +14,38 @@
 				options: ['sm', 'md', 'lg'],
 			},
 		},
-	}} />
+	}
+</script>
+
+<Meta title="GoogleMap" component={GoogleMap} {argTypes} />
 
 <Template let:args>
-	<GoogleMap {apiKey} {...args} />
+	<GoogleMap apiKey={api_Key} {...args} />
 </Template>
 
 <Story name="default" />
+
+<Story name="apiKey" args={{ apiKey: '....' }} let:args>
+	Should break
+	<GoogleMap {...args} />
+</Story>
+
+<Story name="center" args={{ center: { latitude: 30, longitude: 30 } }} />
+<Story name="connect" args={{ multiple: true, connect: true }} />
+<Story name="draggable" args={{ draggable: true }} />
+<Story name="height" args={{ height: 'sm' }} />
+<Story name="multiple" args={{ multiple: true }} />
+<Story name="value" args={{ value: { latitude: 30, longitude: 30 } }} />
+
+<Story
+	name="value(multiple)"
+	args={{
+		multiple: true,
+		value: [
+			{ latitude: 30, longitude: 30 },
+			{ latitude: 31, longitude: 31 },
+			{ latitude: 32, longitude: 31 },
+			{ latitude: 31, longitude: 32 },
+			{ latitude: 30, longitude: 31 },
+		],
+	}} />
