9bb7df4c2700f799e267a53afe8a3aadccb73e1b
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-07-18 17:39:29 +0430

    Add Breadcrumb and BreadcrumbItem components

---
+++ src/components/breadcrumbs/Breadcrumb.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { get_current_component } from 'svelte/internal'
+
+	import { forwardEventsBuilder } from '$lib/directives'
+	import { classname, condition } from '$lib/utils'
+
+	/**
+	 * TODO
+	 */
+	export let separator: 'arrows' | 'bullets' | 'dots' | undefined = undefined
+
+	const forwardEevnts = forwardEventsBuilder(get_current_component())
+
+	$: classes = classname('breadcrumb', { separator }, $$props.class)
+</script>
+
+{#if condition($$props)}
+	<ol use:forwardEevnts {...$$restProps} class={classes}>
+		<slot />
+	</ol>
+{/if}
---
+++ src/components/breadcrumbs/BreadcrumbItem.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { get_current_component } from 'svelte/internal'
+
+	import { forwardEventsBuilder } from '$lib/directives'
+	import { classname, condition } from '$lib/utils'
+
+	/**
+	 * TODO
+	 */
+	export let active: boolean = false
+
+	/**
+	 * TODO
+	 */
+	export let href: 'string' | undefined = undefined
+
+	const forwardEevnts = forwardEventsBuilder(get_current_component())
+
+	$: classes = classname('breadcrumb-item', { active }, $$props.class)
+</script>
+
+{#if condition($$props)}
+	<li use:forwardEevnts {...$$restProps} class={classes}>
+		<a {href}>
+			<slot />
+		</a>
+	</li>
+{/if}
