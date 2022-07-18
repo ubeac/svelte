c7a2cd119cc90d28446e99daaa13267da270e0a8
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-07-18 11:21:27 +0430

    add stories for GoogleMap component

---
+++ .storybook/stories/GoogleMap.stories.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+	import { GoogleMap } from '$lib/components'
+
+	let apiKey = 'AIzaSyBJKPIYzpZOPKyhpgvPCKeWPEAoaiQBHJ8'
+</script>
+
+<Meta
+	title="GoogleMap"
+	component={GoogleMap}
+	argTypes={{
+		apiKey: {
+			control: 'text',
+		},
+		height: {
+			control: {
+				type: 'radio',
+				options: ['sm', 'md', 'lg'],
+			},
+		},
+	}} />
+
+<Template let:args>
+	<GoogleMap {apiKey} {...args} />
+</Template>
+
+<Story name="default" />
