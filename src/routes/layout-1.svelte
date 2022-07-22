566c455c3498e851b90555be547a04160ea7ce5e
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-07-22 20:12:59 +0430

    update layout

--- src/routes/layout-1.svelte
+++ src/routes/layout-1.svelte
@@ -1,109 +1,107 @@
 <script>
-	import { Avatar, Button, Card, CardBody, Grid, Icon, Layout } from '$lib/components'
+	import { Avatar, Button, Card, CardBody, FormRadio, Grid, Icon, Layout } from '$lib/components'
 	import GridItem from '$lib/components/grid/GridItem.svelte'
 	import '../styles.css'
 
+	let alignItems = 'center'
 	let show = false
 </script>
 
-<Layout class="layout" header="center">
-	<div slot="header:start">
-		<Grid>
-			<GridItem sm="hide-down">
-				<div class="headers-start">
-					<a class="brand" href="/">Header</a>
-				</div>
-			</GridItem>
-		</Grid>
-	</div>
-	<div slot="header">
-		<Grid justifyContent="between">
-			<GridItem xs="grow" sm="hide-down">
-				<div class="headers white">
-					<Grid gutter="sm" justifyContent="end">
-						<GridItem sm="hide-down">
-							<Button>
-								<Icon name="moon" />
-							</Button>
-						</GridItem>
-						<GridItem sm="hide-down">
-							<Button>
-								<Icon name="bell" />
-							</Button>
-						</GridItem>
+<Layout header="center">
+	<Grid alignItems="center" justifyContent="center" class="header-grid" slot="header:start">
+		<GridItem sm="hide-down">
+			<a class="brand" href="/">Header</a>
+		</GridItem>
+	</Grid>
+	<Grid slot="header" vertical>
+		<GridItem>
+			<Grid justifyContent="between">
+				<GridItem xs="grow" sm="hide-down">
+					<div class="headers white">
+						<Grid gutter="sm" justifyContent="end">
+							<GridItem sm="hide-down">
+								<Button>
+									<Icon name="moon" />
+								</Button>
+							</GridItem>
+							<GridItem sm="hide-down">
+								<Button>
+									<Icon name="bell" />
+								</Button>
+							</GridItem>
 
-						<GridItem sm="hide-down">
-							<Avatar size="sm" shape="circle">PR</Avatar>
-						</GridItem>
-					</Grid>
-				</div>
-			</GridItem>
+							<GridItem sm="hide-down">
+								<Avatar size="sm" shape="circle">PR</Avatar>
+							</GridItem>
+						</Grid>
+					</div>
+				</GridItem>
 
-			<GridItem md="hide-up" xs="grow">
-				<div class="headers blue">
-					<Grid alignItems="center" justifyContent="between">
-						<GridItem md="hide-up">
-							<Button color="secondary" outline on:click={() => (show = !show)}>
-								<Icon pack="la" name="bars" />
-							</Button>
-						</GridItem>
-						<GridItem md="hide-up">
-							<a class="brand" href="/">Header</a>
-						</GridItem>
-						<GridItem md="hide-up">
-							<Avatar size="sm" shape="circle">PR</Avatar>
-						</GridItem>
-					</Grid>
-				</div>
-			</GridItem>
-		</Grid>
+				<GridItem md="hide-up" xs="grow">
+					<div class="headers blue">
+						<Grid alignItems="center" justifyContent="between">
+							<GridItem md="hide-up">
+								<Button color="secondary" outline on:click={() => (show = !show)}>
+									<Icon pack="la" name="bars" />
+								</Button>
+							</GridItem>
+							<GridItem md="hide-up">
+								<a class="brand" href="/">Header</a>
+							</GridItem>
+							<GridItem md="hide-up">
+								<Avatar size="sm" shape="circle">PR</Avatar>
+							</GridItem>
+						</Grid>
+					</div>
+				</GridItem>
+			</Grid>
+		</GridItem>
 
-		<Grid if={show}>
-			<GridItem xs="grow" alignSelf="stretch">
-				<div class="aside-items">
-					<div class="aside-item">item</div>
-					<div class="aside-item">item</div>
-					<div class="aside-item">item</div>
-					<div class="aside-item">item</div>
-					<div class="aside-item">item</div>
-					<div class="aside-item">item</div>
-					<div class="aside-item">item</div>
-				</div>
-			</GridItem>
-		</Grid>
-	</div>
-	<div class="aside-start" slot="aside:start">
-		<Grid>
-			<GridItem sm="hide-down">
-				<div>
-					<div class="aside-item">item</div>
-					<div class="aside-item">item</div>
-					<div class="aside-item">item</div>
-					<div class="aside-item">item</div>
-					<div class="aside-item">item</div>
-					<div class="aside-item">item</div>
-					<div class="aside-item">item</div>
-				</div>
-			</GridItem>
-		</Grid>
-	</div>
-	<div class="main">
-		<Card>
-			<CardBody>
-				<a href="https://preview.tabler.io/layout-combo.html">Reference layout</a>
-			</CardBody>
-			<CardBody>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda labore deserunt maiores adipisci earum vitae
-				voluptatum, necessitatibus dolore dicta consequatur modi soluta voluptatem. Est vitae consequatur fugit dolor
-				quam? Earum!
-			</CardBody>
-			<CardBody>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda labore deserunt maiores adipisci earum vitae
-				voluptatum, necessitatibus dolore dicta consequatur modi soluta voluptatem. Est vitae consequatur fugit dolor
-				quam? Earum!
-			</CardBody>
-		</Card>
-	</div>
+		<GridItem if={show} class="aside-items" xs="grow">
+			<div class="aside-item">item</div>
+			<div class="aside-item">item</div>
+			<div class="aside-item">item</div>
+			<div class="aside-item">item</div>
+			<div class="aside-item">item</div>
+			<div class="aside-item">item</div>
+			<div class="aside-item">item</div>
+		</GridItem>
+	</Grid>
+
+	<Grid slot="aside:start">
+		<GridItem sm="hide-down">
+			<div class="aside-item">item</div>
+			<div class="aside-item">item</div>
+			<div class="aside-item">item</div>
+			<div class="aside-item">item</div>
+			<div class="aside-item">item</div>
+			<div class="aside-item">item</div>
+			<div class="aside-item">item</div>
+		</GridItem>
+	</Grid>
+	<Grid {alignItems} justifyContent="center" class="app-content">
+		<GridItem>
+			<Card class="app-body" outline>
+				<CardBody>
+					<a href="https://preview.tabler.io/layout-combo.html">Reference layout</a>
+				</CardBody>
+				<CardBody>
+					AppContent:mode
+					<FormRadio value="stretch" label="stretch" bind:group={alignItems} />
+					<FormRadio value="center" label="center" bind:group={alignItems} />
+					<FormRadio value="start" label="start" bind:group={alignItems} />
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda labore deserunt maiores adipisci earum vitae
+					voluptatum, necessitatibus dolore dicta consequatur modi soluta voluptatem. Est vitae consequatur fugit dolor quam?
+					Earum!
+				</CardBody>
+				<CardBody>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda labore deserunt maiores adipisci earum
+					vitae voluptatum, necessitatibus dolore dicta consequatur modi soluta voluptatem. Est vitae consequatur fugit
+					dolor quam? Earum!
+				</CardBody>
+			</Card>
+		</GridItem>
+	</Grid>
 </Layout>
 
 <style global>
@@ -111,10 +109,9 @@
 		margin: 0;
 	}
 
-	.u-grid > div {
-		width: 100%;
+	.header-grid {
+		height: 100%;
 	}
-
 	.layout {
 		background-color: #efefef;
 	}
@@ -124,41 +121,38 @@
 	.blue {
 		background-color: #32374d;
 	}
-
-	.header-start {
-		background-color: #32374d;
-
-		display: flex;
-		flex-direction: column;
-		align-items: center;
+	.aside-item {
+		min-width: 240px;
 	}
-
-	.headers-start {
-		height: 100%;
-		/* padding: 1rem; */
-		width: 240px;
+	.header-start {
 		font-weight: bold;
 		background-color: #32374d;
-		display: flex;
-		align-items: center;
-		justify-content: center;
 	}
 	.brand {
-		padding-top: 1rem;
 		font-size: 32px;
 		color: white;
 	}
 	.brand-dark {
-		padding-top: 1rem;
 		font-size: 32px;
 		color: #32374d;
 	}
+
 	.main {
-		padding: 0.5rem;
+		height: 100%;
+	}
+
+	.app-content {
+		height: 100%;
+		padding: 1rem;
+	}
+
+	.app-body {
+		height: 100%;
 	}
 	.aside-items {
 		background-color: #32374d;
 	}
+
 	.aside-item {
 		padding: 0.5rem;
 		color: white;
@@ -177,8 +171,6 @@
 	}
 
 	.aside-start {
-		display: flex;
-		flex-direction: column;
 		background-color: #32374d;
 	}
 </style>
