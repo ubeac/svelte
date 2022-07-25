178105cb51ec79f81cd64713af8d463a37f36322
Masood Abdolian <m.abdolian@gmail.com>
2022-07-25 15:58:41 +0430

    add popperjs

---
+++ src/components/tooltip/Tooltip.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { get_current_component } from 'svelte/internal'
+
+	import { createPopper, Instance } from '@popperjs/core'
+
+	import { forwardEventsBuilder } from '$lib/directives'
+	import { classname, condition } from '$lib/utils'
+
+	/**
+	 * TODO
+	 */
+	export let placement: '' = undefined
+
+	const forwardEvents = forwardEventsBuilder(get_current_component())
+
+	$: classes = classname('tooltip', { placement }, $$props.class)
+</script>
+
+{#if condition($$props)}{/if}
+<button
+	type="button"
+	class="btn"
+	data-bs-toggle="tooltip"
+	data-bs-html="true"
+	title="<em>Tooltip</em> <u>with</u> <b>HTML</b>">
+	Tooltip with HTML
+</button>
