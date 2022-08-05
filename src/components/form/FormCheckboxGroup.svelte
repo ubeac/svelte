7781a90888e64745d7ae3f64b27594946ee01a58
Masood Abdolian <m.abdolian@gmail.com>
2022-08-05 23:58:25 +0430

    init FormCheckboxGroup

---
+++ src/components/form/FormCheckboxGroup.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { get_current_component } from 'svelte/internal'
+
+	import { FormCheckbox } from '$lib/components'
+	import { forwardEventsBuilder } from '$lib/directives'
+
+	const forwardEvents = forwardEventsBuilder(get_current_component())
+</script>
+
+<FormCheckbox {forwardEvents} />
