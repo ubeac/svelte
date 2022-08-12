25e06a1e43e91412d08534533998d5ac942ba6d4
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-08-12 06:31:21 +0000

    Update stories of Radio

--- .storybook/stories/Radio.stories.svelte
+++ .storybook/stories/Radio.stories.svelte
@@ -10,13 +10,35 @@
 		group: {
 			control: 'text',
 		},
+	}}
+	parameters={{
+		actions: {
+			handles: ['changed'],
+		},
 	}} />
 
-<Template let:args>
+<Story name="Default" let:args>
 	<Radio name="radio" value="radio1" {...args} />
 	<Radio name="radio" value="radio2" {...args} />
 	<Radio name="radio" value="radio3" {...args} />
-</Template>
+</Story>
 
-<Story name="default" />
-<Story name="group" args={{ group: 'radio2' }} />
+<Story name="Group" args={{ group: 'radio2' }} let:args>
+	<Radio name="radio" value="radio1" {...args} />
+	<Radio name="radio" value="radio2" {...args} />
+	<Radio name="radio" value="radio3" {...args} />
+</Story>
+
+<Story name="Value">
+	<div class="example-radios">
+		<Radio />
+		<Radio value />
+	</div>
+</Story>
+
+<Story name="Disabled">
+	<div class="example-radios">
+		<Radio disabled />
+		<Radio value disabled />
+	</div>
+</Story>
