206900be5c28e505c232ead1d796b462ffe46a16
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-08-19 00:16:56 +0430

    add stories for Menu

---
+++ .storybook/stories/Menu.stories.svelte
@@ -0,0 +1,64 @@
+<script>
+	import { Meta, Story } from '@storybook/addon-svelte-csf'
+	import { Menu, Button } from '$lib/components'
+
+	const PLACEMENTS = [
+		'auto',
+		'auto-start',
+		'auto-end',
+		'right',
+		'right-start',
+		'right-end',
+		'left',
+		'left-start',
+		'left-end',
+		'top',
+		'top-start',
+		'top-end',
+		'bottom',
+		'bottom-start',
+		'bottom-end',
+	]
+</script>
+
+<Meta title="Menu" component={Menu} />
+
+<Story name="Default" let:args>
+	<Button>Dropdown</Button>
+	<Menu {...args}>
+		<li class="nav-link">Item 1</li>
+		<li class="nav-link">Item 2</li>
+		<li class="nav-link">Item 3</li>
+	</Menu>
+</Story>
+
+<Story name="Offset" let:args>
+	<Button>Dropdown</Button>
+	<Menu offset={[16, 16]} {...args}>
+		<li class="nav-link">Item 1</li>
+		<li class="nav-link">Item 2</li>
+		<li class="nav-link">Item 3</li>
+	</Menu>
+</Story>
+
+<Story name="Persistence" let:args>
+	<Button>Dropdown</Button>
+	<Menu persistence {...args}>
+		<li class="nav-link">Item 1</li>
+		<li class="nav-link">Item 2</li>
+		<li class="nav-link">Item 3</li>
+	</Menu>
+</Story>
+
+<Story name="Placement" let:args>
+	<div class="example-menu">
+		{#each PLACEMENTS as placement}
+			<Button>{placement}</Button>
+			<Menu {placement} {...args}>
+				<li class="nav-link">Item 1</li>
+				<li class="nav-link">Item 2</li>
+				<li class="nav-link">Item 3</li>
+			</Menu>
+		{/each}
+	</div>
+</Story>
