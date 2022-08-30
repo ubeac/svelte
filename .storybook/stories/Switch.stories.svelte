88324aa6cf509a0a9e5fa39a99fc13357e945cd9
Mohammadsaleh <mohammadsalehkarimi@outlook.com>
2022-08-30 20:23:14 +0430

    #240 Switch - review and fix functionality

--- .storybook/stories/Switch.stories.svelte
+++ .storybook/stories/Switch.stories.svelte
@@ -1,22 +1,36 @@
 <script>
 	import { Meta, Story } from '@storybook/addon-svelte-csf'
 	import { Switch } from '$lib/components'
-	import { COLORS } from '$lib/types';
+	import { COLORS } from '$lib/types'
 </script>
 
-<Meta component={Switch} title="Switch" argTypes={{ value: { control: 'boolean' } , color: {
-	defaultValue: 'auto',
-	control: {
-		type: 'select',
-		default: 'primary',
-		options: COLORS,
-	},
-}, }} />
+<Meta
+	component={Switch}
+	title="Switch"
+	argTypes={{
+		value: { control: 'boolean' },
+		label: {
+			control: 'text',
+		},
+		color: {
+			defaultValue: 'auto',
+			control: {
+				type: 'select',
+				default: 'primary',
+				options: COLORS,
+			},
+		},
+	}} />
 
 <Story name="Default" let:args>
 	<Switch {...args} />
 </Story>
 
+<Story name="Colors" let:args>
+	{#each COLORS as color}
+		<Switch {...args} {color} label={color} value />
+	{/each}
+</Story>
 
 <Story name="Disabled" let:args>
 	<Switch disabled {...args} />
@@ -24,8 +38,4 @@
 
 <Story name="Value" let:args>
 	<Switch value {...args} />
-</Story>
-
-<Story name="color:dagner" let:args>
-	<Switch value {...args} color="danger" />
 </Story>
