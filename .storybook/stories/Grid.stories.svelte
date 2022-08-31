c32b3e7279393a5372186c7bca5e636c9530c9ea
armin <armin@smileitsolutions.uk>
2022-08-31 18:59:01 +0430

    #314 fix part of story

--- .storybook/stories/Grid.stories.svelte
+++ .storybook/stories/Grid.stories.svelte
@@ -1,96 +1,142 @@
 <script>
-	import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
-
-	import { Grid, GridItem } from '$lib/components'
-
-	const ALIGN_CONTENTS = ['start', 'center', 'end', 'stretch', 'between', 'around', 'evenly', undefined]
-	const ALIGN_ITEMS = ['start', 'center', 'end', 'stretch', undefined]
-	const GUTTERS = ['xs', 'sm', 'md', 'lg', 'xl', 'xxl', undefined]
-	const JUSTIFY_CONTENTS = ['start', 'center', 'end', 'between', 'around', 'evenly', undefined]
-
-	function control(type, options) {
-		return {
-			control: {
-				type,
-				options,
-			},
-		}
-	}
-
-	const argTypes = {
-		alignContent: control('select', ALIGN_CONTENTS),
-		alignContentXs: control('select', ALIGN_CONTENTS),
-		alignContentSm: control('select', ALIGN_CONTENTS),
-		alignContentMd: control('select', ALIGN_CONTENTS),
-		alignContentLg: control('select', ALIGN_CONTENTS),
-		alignContentXl: control('select', ALIGN_CONTENTS),
-		alignContentXxl: control('select', ALIGN_CONTENTS),
-
-		alignItems: control('select', ALIGN_ITEMS),
-		alignItemsXs: control('select', ALIGN_ITEMS),
-		alignItemsSm: control('select', ALIGN_ITEMS),
-		alignItemsMd: control('select', ALIGN_ITEMS),
-		alignItemsLg: control('select', ALIGN_ITEMS),
-		alignItemsXl: control('select', ALIGN_ITEMS),
-		alignItemsXxl: control('select', ALIGN_ITEMS),
-
-		justifyContent: control('select', JUSTIFY_CONTENTS),
-		justifyContentXs: control('select', JUSTIFY_CONTENTS),
-		justifyContentSm: control('select', JUSTIFY_CONTENTS),
-		justifyContentMd: control('select', JUSTIFY_CONTENTS),
-		justifyContentLg: control('select', JUSTIFY_CONTENTS),
-		justifyContentXl: control('select', JUSTIFY_CONTENTS),
-		justifyContentXxl: control('select', JUSTIFY_CONTENTS),
-
-		gutter: control('select', GUTTERS),
-		gutterX: control('select', GUTTERS),
-		gutterY: control('select', GUTTERS),
-		reverse: control('boolean'),
-		vertical: control('boolean'),
-		wrap: control('check', [false, true, 'reverse']),
-		wrapXs: control('check', [false, true, 'reverse']),
-		wrapSm: control('check', [false, true, 'reverse']),
-		wrapMd: control('check', [false, true, 'reverse']),
-		wrapLg: control('check', [false, true, 'reverse']),
-		wrapXl: control('check', [false, true, 'reverse']),
-		wrapXxl: control('check', [false, true, 'reverse']),
-	}
+	import { Meta, Story } from '@storybook/addon-svelte-csf'
+	import { Container, Row, Col } from '$lib/components/gird'
+	import { Card, CardBody } from '$lib/components/card'
+	import { GRID_ALIGN_CONTENT, GRID_ALIGN_ITEMS } from '$lib/components/gird/grid.types'
 </script>
 
-<Meta title="Grid" component={Grid} {argTypes} />
+<Meta title="Grid" />
+<Story name="default" let:args>
+	<Container>
+		<Row align="center">
+			<Card>
+				<CardBody>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat deserunt libero nesciunt. Sed dignissimos
+						laboriosam eaque non aliquid ipsam architecto facilis, delectus molestias. Laborum tempore aliquid, in quas
+						amet reprehenderit.
+					</p>
+				</CardBody>
+			</Card>
+		</Row>
+	</Container>
+</Story>
 
-<Template let:args>
-	<Grid class="h-5" {...args}>
-		<GridItem class="h-4 w-4 d-flex justify-content-center align-items-center bg-red">1</GridItem>
-		<GridItem class="h-4 w-4 d-flex justify-content-center align-items-center bg-yellow">2</GridItem>
-		<GridItem class="h-4 w-4 d-flex justify-content-center align-items-center bg-green">3</GridItem>
-		<GridItem class="h-4 w-4 d-flex justify-content-center align-items-center bg-blue">4</GridItem>
-	</Grid>
-</Template>
+<Story name="Align">
+	{#each ['start', 'center', 'end'] as align}
+		<Container class="container-example">
+			<Row class="row-example" {align}>
+				{#each [1, 2, 3] as item}
+					<Col>
+						<Card class="card-example">One of three columns</Card>
+					</Col>
+				{/each}
+			</Row>
+		</Container>
+	{/each}
+</Story>
 
-<Story name="default" />
-<Story name="Align Content" args={{ alignContent: 'start' }} />
-<Story name="Align Content XS" args={{ alignContentXs: 'start' }} />
-<Story name="Align Content SM" args={{ alignContentSm: 'start' }} />
-<Story name="Align Content MD" args={{ alignContentMd: 'start' }} />
-<Story name="Align Content LG" args={{ alignContentLg: 'start' }} />
-<Story name="Align Content XL" args={{ alignContentXl: 'start' }} />
-<Story name="Align Content XXL" args={{ alignContentXxl: 'start' }} />
+<Story name="Align Self">
+	<Container class="container-example">
+		<Row class="row-example" noGutters>
+			{#each ['start', 'center', 'end'] as align}
+				<Col alignSelf={align}>
+					<Card class="card-example">One of three columns</Card>
+				</Col>
+			{/each}
+		</Row>
+	</Container>
+</Story>
 
-<Story name="Justify Content" args={{ justifyContent: 'start' }} />
-<Story name="Justify Content XS" args={{ justifyContentXs: 'start' }} />
-<Story name="Justify Content SM" args={{ justifyContentSm: 'start' }} />
-<Story name="Justify Content MD" args={{ justifyContentMd: 'start' }} />
-<Story name="Justify Content LG" args={{ justifyContentLg: 'start' }} />
-<Story name="Justify Content XL" args={{ justifyContentXl: 'start' }} />
-<Story name="Justify Content XXL" args={{ justifyContentXxl: 'start' }} />
+<Story name="Breakpoint sizing">
+	<Container class="container-example">
+		<Row class="row-example" noGutters>
+			<Col>
+				<Card class="card-example">1 of 2</Card>
+			</Col>
+			<Col>
+				<Card class="card-example">2 of 2</Card>
+			</Col>
+		</Row>
+		<Row class="row-example" noGutters>
+			<Col>
+				<Card class="card-example">1 of 3</Card>
+			</Col>
+			<Col>
+				<Card class="card-example">2 of 3</Card>
+			</Col>
+			<Col>
+				<Card class="card-example">3 of 3</Card>
+			</Col>
+		</Row>
+	</Container>
+</Story>
 
-<Story name="Justify Items" args={{ justifyItems: 'start' }} />
-<Story name="Justify Items XS" args={{ justifyItemsXs: 'start' }} />
-<Story name="Justify Items SM" args={{ justifyItemsSm: 'start' }} />
-<Story name="Justify Items MD" args={{ justifyItemsMd: 'start' }} />
-<Story name="Justify Items LG" args={{ justifyItemsLg: 'start' }} />
-<Story name="Justify Items XL" args={{ justifyItemsXl: 'start' }} />
-<Story name="Justify Items XXL" args={{ justifyItemsXxl: 'start' }} />
+<Story name="Justify">
+	<Container class="container-example">
+		{#each ['start', 'center', 'end', 'around', 'between'] as justify}
+			<Row class="row-example" noGutters {justify}>
+				<Col cols="4">
+					<Card class="card-example">1 of 2</Card>
+				</Col>
+				<Col cols="4">
+					<Card class="card-example">2 of 2</Card>
+				</Col>
+			</Row>
+		{/each}
+	</Container>
+</Story>
 
-<Story name="reverse" args={{ reverse: true }} />
+<Story name="No gutters">
+	<Container class="container-example">
+		<Row class="row-example" noGutters>
+			<Col cols="12" sm="6" md="8">
+				<Card class="card-example">.col-12 .col-sm-6 .col-md-8</Card>
+			</Col>
+			<Col cols="6" md="4">
+				<Card class="card-example">.col-6 .col-md-4</Card>
+			</Col>
+		</Row>
+	</Container>
+</Story>
+
+<Story name="Offset">
+	<Container class="container-example">
+		<Row class="row-example" noGutters>
+			<Col md="4">
+				<Card class="card-example">.col-md-4</Card>
+			</Col>
+			<Col md="4" offsetMd="4">
+				<Card class="card-example">.col-md-4 .offset-md-4</Card>
+			</Col>
+		</Row>
+		<Row class="row-example" noGutters>
+			<Col md="3" offsetMd="3">
+				<Card class="card-example">.col-md-3 .offset-md-3</Card>
+			</Col>
+			<Col md="3" offsetMd="3">
+				<Card class="card-example">.col-md-3 .offset-md-3</Card>
+			</Col>
+		</Row>
+		<Row class="row-example" noGutters>
+			<Col md="6" offsetMd="3">
+				<Card class="card-example">.col-md-6 .offset-md-3</Card>
+			</Col>
+		</Row>
+	</Container>
+</Story>
+
+<style global>
+	.container-example {
+		min-height: 150px;
+		background: #fafafa;
+		margin-bottom: 24px;
+	}
+	.row-example {
+		min-height: 150px;
+	}
+
+	.card-example {
+		padding: 8px;
+	}
+</style>
