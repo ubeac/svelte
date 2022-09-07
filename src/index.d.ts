e306603e902f4f81e8e255454e645e4c40bbc244
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-09-07 19:39:56 +0430

    #377 - move if declaration to types folder

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,10 +1,2 @@
 export * from './components'
-/// <reference types="@sveltejs/kit" />
-
-declare global {
-	namespace svelte.JSX {
-		interface HTMLProps<T> {
-			if?: boolean
-		}
-	}
-}
+export * from './types'
