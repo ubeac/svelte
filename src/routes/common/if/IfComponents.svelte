6827d137e5559f2f44f5322a6bdf08f0ff98beaa
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-09-09 01:31:36 +0430

    #403 - update examples of if prop

---
+++ src/routes/common/if/IfComponents.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { Input } from '@ubeac/svelte'
+
+	/** @type {string} */
+	let value
+</script>
+
+<Input if={2 + 2 == 4} placeholder="Only shown if 2 + 2 == 4" bind:value />
+<Input if={2 + 2 == 5} placeholder="You cannot see me" />
+
+<p if={!!value}>You typed: {value}</p>
---
+++ src/routes/common/if/IfNative.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { Button } from '@ubeac/svelte'
+
+	let show = false
+	const toggle = () => (show = !show)
+</script>
+
+<Button on:click={toggle}>Toggle</Button>
+
+<div class="examples">
+	<div if={show}>this is content of div</div>
+	<button if={show}>this is button</button>
+	<p if={show}>this is p</p>
+	<input if={show} placeholder="This is input" />
+</div>
+
+<style>
+	.examples {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+	}
+</style>
