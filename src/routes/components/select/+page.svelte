70db44da6c494e398eaf4722ddf9221a8c316e49
Mohammadsaleh <mohammadsalehkarimi@outlook.com>
2022-09-10 00:19:37 +0430

    Implement FormSelect Component Document

--- src/routes/components/select/+page.svelte
+++ src/routes/components/select/+page.svelte
@@ -1,5 +1,8 @@
 <script>
 	import { Preview } from '@ubeac/svelte'
+	import FormSelectDefault from './FormSelectDefault.svelte'
+	import FormSelectIcon from './FormSelectIcon.svelte'
+	import FormSelectLoading from './FormSelectLoading.svelte'
 	import SelectDefault from './SelectDefault.svelte'
 	import SelectMultiple from './SelectMultiple.svelte'
 	import SelectPlaceholder from './SelectPlaceholder.svelte'
@@ -26,8 +29,36 @@
 	<SelectMultiple />
 </Preview>
 
+<h1>FormSelect</h1>
+<p>Add one of the available selects a dropdown select - to let users choose from a predefined set of options.</p>
+<Preview src="./FormSelectDefault.svelte">
+	<div class="example-formselect">
+		<FormSelectDefault />
+	</div>
+</Preview>
+
+<h1>FormSelect Loading</h1>
+<p>Select can have loading.</p>
+<Preview src="./FormSelectLoading.svelte">
+	<div class="example-formselect">
+		<FormSelectLoading />
+	</div>
+</Preview>
+
+<h1>FormSelect Icon</h1>
+<p>Select can have icon.</p>
+<Preview src="./FormSelectIcon.svelte">
+	<div class="example-formselect">
+		<FormSelectIcon />
+	</div>
+</Preview>
+
 <style>
 	h1 {
 		margin-top: 20px;
+	}
+
+	.example-formselect {
+		width: 100%;
 	}
 </style>
---
+++ src/routes/components/select/FormSelectDefault.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { FormSelect } from '@ubeac/svelte'
+
+	let items = ['Apple', 'Orange', 'Peach', 'Banana', 'Apricot']
+	let value = ''
+</script>
+
+<FormSelect bind:value {items} label="label" />
