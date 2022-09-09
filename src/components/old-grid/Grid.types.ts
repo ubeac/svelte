decf3fd6238e305bbf8ace1154df38ceb7d2c94b
armin <armin@smileitsolutions.uk>
2022-09-09 16:50:26 +0430

    remove old grid

--- src/components/old-grid/Grid.types.ts
+++
@@ -1,7 +0,0 @@
-export type GridAlignContent = 'start' | 'center' | 'end' | 'stretch' | 'between' | 'around' | 'evenly' | undefined
-export type GridAlignItems = 'start' | 'center' | 'end' | 'stretch' | undefined
-export type GridGutter = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | undefined
-export type GridJustifyContent = 'start' | 'center' | 'end' | 'between' | 'around' | 'evenly' | undefined
-export type GridReverse = boolean | undefined
-export type GridVertical = boolean | undefined
-export type GridWrap = boolean | 'reverse' | undefined
--- src/components/old-grid/GridItem.types.ts
+++
@@ -1,7 +0,0 @@
-type Numbers = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12'
-
-export type GridItemAlignSelf = 'auto' | 'stretch' | 'start' | 'center' | 'end' | undefined
-export type GridItemColumn = Numbers | 'auto' | 'grow' | 'hide' | 'hide-up' | 'hide-down' | undefined
-export type GridItemOffset = Numbers | undefined
-export type GridItemOrder = Numbers | undefined
-export type GridItemHide = boolean | undefined
