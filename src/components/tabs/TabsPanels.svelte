a6b5902b57244a6f38e845337b862e810830db1e
Masood Abdolian <m.abdolian@gmail.com>
2022-09-09 17:14:09 +0430

    update tabs

--- src/components/tabs/TabsPanels.svelte
+++ src/components/tabs/TabsPanels.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
+	import { get_current_component } from 'svelte/internal'
+
+	import { forwardEventsBuilder } from '$lib/directives'
+	import { classname } from '$lib/utils'
+
+	/**
+	 * Forward All native Events.
+	 */
+	export let forwardEvents = forwardEventsBuilder(get_current_component())
+
+	$: classes = classname('tabs-panels', undefined, $$props.class, true)
 </script>
 
-<div>
+<div use:forwardEvents {...$$restProps} class={classes}>
 	<slot />
 </div>
