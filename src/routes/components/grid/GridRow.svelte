5893689fef9f5304c9b8f26d8f77d036b4dc27ed
armin <armin@smileitsolutions.uk>
2022-09-10 12:38:31 +0430

    fix dynamic change cols

--- src/routes/components/grid/GridRow.svelte
+++ src/routes/components/grid/GridRow.svelte
@@ -1,13 +1,17 @@
 <script>
 	import { Container, Row, Col } from '$lib/components/gird'
 	import { Card, CardBody } from '$lib/components/card'
+
+	let innerWidth = 0
+	$: cols = innerWidth > 1200 && innerWidth < 1400 ? [3, 9] : innerWidth < 992 && innerWidth > 576 ? [9, 3] : [6, 6]
 </script>
 
+<svelte:window bind:innerWidth />
 <Container class="container-example">
 	<Row noGutters>
 		{#each [1, 2] as col}
-			<Col :lg="cols[n - 1]" :md="6" :sm="cols[n - 1]">
-				<Card class="card-example">col-6</Card>
+			<Col lg={cols[col - 1]} md="6" sm={cols[col - 1]}>
+				<Card class="card-example">col-{cols[col - 1]}</Card>
 			</Col>
 		{/each}
 	</Row>
