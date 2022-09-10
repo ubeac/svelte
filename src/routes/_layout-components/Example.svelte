1c21a4d5a60eb7d0ffb72cb288cf4de2f09c16b2
Amir Pournasserian <ap@momentaj.com>
2022-09-10 14:09:30 -0400

    #435 organizing documentation routes and folders

--- src/routes/_layout-components/Example.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import { Card } from '$lib/components'
-</script>
-
-<Card outline class="example-box">
-	<slot />
-</Card>
---
+++ src/routes/docs/components/buttons/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { Preview } from '$lib/components'
+
+	import ButtonColors from './ButtonColors.svelte'
+	import ButtonDefault from './ButtonDefault.svelte'
+</script>
+
+<h1>Buttons</h1>
+
+<h2>Default</h2>
+<p>
+	Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias eligendi quaerat eveniet minus et explicabo doloribus
+	dolorem dolor, tenetur voluptatibus? Accusantium, quam porro? Culpa amet sint illo sequi labore cum.
+</p>
+<Preview src="./ButtonDefault.svelte">
+	<ButtonDefault />
+</Preview>
+
+<h2>Colors</h2>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim eos deserunt eveniet obcaecati natus voluptatum
+	inventore mollitia quam harum facilis? Repellendus explicabo maxime exercitationem recusandae quo dolorem itaque at
+	dignissimos.
+</p>
+<Preview vertical src="./ButtonColors.svelte">
+	<ButtonColors />
+</Preview>
