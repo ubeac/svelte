960ccc612fd08377bd024aab2ddf4cb9034d2dfa
armin <armin@smileitsolutions.uk>
2022-09-10 19:01:34 +0430

    #428 page component

---
+++ src/routes/components/page/PageHeaderAction.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { Page } from '@ubeac/svelte/components/page'
+	import { Button } from '@ubeac/svelte/components/button'
+</script>
+
+<Page>
+	<svelte:fragment slot="headerTitle">Edit Product</svelte:fragment>
+	<svelte:fragment slot="headerActions">
+		<Button color="primary">update</Button>
+		<Button color="danger">close</Button>
+	</svelte:fragment>
+</Page>
