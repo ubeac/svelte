24ed4bf07f68cd9bf6d58d557f3382c8a5e19740
Masood Abdolian <m.abdolian@gmail.com>
2022-09-10 23:28:51 +0430

    clean the page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,3 @@
 <script lang="ts">
-	import { Tabs, TabsItem, TabsItems, TabsPanel, TabsPanels } from '$lib/components/tabs'
-
-	let value = '1'
-
-	let v = '3'
+	// import { } from '$lib/components'
 </script>
-
-<button on:click={() => (value = '5')}>change</button>
-<Tabs value="3">
-	<TabsItems>
-		<TabsItem {value}>Button 1</TabsItem>
-		<TabsItem value="2">Button 2</TabsItem>
-		<TabsItem value="3">Button 3</TabsItem>
-	</TabsItems>
-	<TabsPanels>
-		<TabsPanel {value}>Panel 1</TabsPanel>
-		<TabsPanel value="2">Panel 2</TabsPanel>
-		<TabsPanel value="3">Panel 3</TabsPanel>
-	</TabsPanels>
-</Tabs>
