b3f03408b42b35292220868ce0d3327f4151a018
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-09-12 23:04:19 +0430

    #469 - write docs for Label component

---
+++ src/routes/docs/components/label/LabelRequired.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { Container, Row, Col, Input, Label } from '@ubeac/svelte/components'
+</script>
+
+<Container>
+	<Row>
+		<Col>
+			<Label required for="input-1">Required field</Label>
+			<Input required id="input-1" />
+		</Col>
+		<Col>
+			<Label for="input-1">Optional field</Label>
+			<Input id="input-1" />
+		</Col>
+	</Row>
+</Container>
