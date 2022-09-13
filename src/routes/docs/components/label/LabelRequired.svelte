3bd7113691279dd34a60c9e1832f7d9fdb0fce3e
armin <armin@smileitsolutions.uk>
2022-09-13 16:06:26 +0430

    #469  fix id and label

--- src/routes/docs/components/label/LabelRequired.svelte
+++ src/routes/docs/components/label/LabelRequired.svelte
@@ -5,12 +5,12 @@
 <Container>
 	<Row>
 		<Col>
-			<Label required for="input-1">Required field</Label>
-			<Input required id="input-1" />
+			<Label required for="input-2">Required field</Label>
+			<Input required id="input-2" />
 		</Col>
 		<Col>
-			<Label for="input-1">Optional field</Label>
-			<Input id="input-1" />
+			<Label for="input-3">Optional field</Label>
+			<Input id="input-3" />
 		</Col>
 	</Row>
 </Container>
