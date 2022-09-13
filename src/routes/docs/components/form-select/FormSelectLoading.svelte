e7360bdcf1e6fcb73cd13c6b5abd7e072cb5d93e
Mohammadsaleh <mohammadsalehkarimi@outlook.com>
2022-09-13 19:31:05 +0430

    Update Form Select Document

--- src/routes/docs/components/form-select/FormSelectLoading.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import { FormSelect } from '@ubeac/svelte'
-
-	let items = ['Apple', 'Orange', 'Peach', 'Banana', 'Apricot']
-	let value = ''
-</script>
-
-<FormSelect bind:value {items} loading label="loading" />
