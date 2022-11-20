ea3e69db3c4b97d9f38593a6267116d72481cd4e
armin <armin@smileitsolutions.uk>
2022-11-20 10:16:01 +0330

    commit All

--- src/components/Accordion/Accordion.svelte
+++ src/components/Accordion/Accordion.svelte
@@ -5,36 +5,46 @@
 	import { nanoid } from 'nanoid'
 
 	import { forwardEventsBuilder } from '$lib/directives'
-	import { classname } from '$lib/utils'
+
+	import { El } from '../base'
+	import type { SharedProps } from '../base/El.type'
+	import type { AccordionContext, AccordionProps, AccordionsContext } from './Accordion.types'
+
+	type $$Props = AccordionProps
 
 	/**
 	 * Id of Accordion
 	 */
-	export let id: string = nanoid()
+	export let id: $$Props['id'] = nanoid()
 
 	/**
 	 * Controls open/close state of Accordion Item
 	 */
-	export let open: boolean = false
+	export let open: $$Props['open'] = false
 
-	const context = writable({ id, open })
-
-	const forwardEvents = forwardEventsBuilder(get_current_component())
+	const context: AccordionContext = writable({ id, open })
 
 	setContext('ACCORDION', context)
 
-	const acccordions = getContext<any>('ACCORDIONS')
+	const acccordions: AccordionsContext = getContext('ACCORDIONS')
 
 	acccordions.update((item: any) => {
 		item.children.push(context)
 		return item
 	})
 
-	$: classes = classname('accordion-item', undefined, $$props.class)
+	let props: SharedProps = {}
+	$: props = {
+		...$$restProps,
+		forwardEvents: forwardEventsBuilder(get_current_component()),
+		cssPrefix: 'accordion-item',
+		tag: 'div',
+		cssProps: {},
+	}
 
 	$: context.set({ id, open })
 </script>
 
-<div use:forwardEvents {...$$restProps} class={classes}>
+<El {...props}>
 	<slot />
-</div>
+</El>
--- src/components/Accordion/Accordions.svelte
+++ src/components/Accordion/Accordions.svelte
@@ -5,25 +5,37 @@
 	import { nanoid } from 'nanoid'
 
 	import { forwardEventsBuilder } from '$lib/directives'
-	import { classname } from '$lib/utils'
+
+	import { El } from '../base'
+	import type { SharedProps } from '../base/El.type'
+	import type { AccordionsProps } from './Accordion.types'
+
+	type $$Props = AccordionsProps
 
 	/**
 	 * Id of Accordion group
 	 */
-	export let group: string = nanoid()
+	export let group: $$Props['group'] = nanoid()
 
 	/**
 	 * Let more than one Accordion be open in same time
 	 */
-	export let persistent: boolean = false
+	export let persistent: $$Props['persistent'] = false
 
 	const context = writable({ group, persistent, children: [] })
 
-	const forwardEvents = forwardEventsBuilder(get_current_component())
-
 	setContext('ACCORDIONS', context)
 
-	$: classes = classname('accordion', undefined, $$props.class)
+	let props: SharedProps = {}
+
+	$: props = {
+		...$$restProps,
+		id: group,
+		forwardEvents: forwardEventsBuilder(get_current_component()),
+		cssPrefix: 'accordion',
+		tag: 'div',
+		cssProps: {},
+	}
 
 	$: context.update((item) => {
 		item.group = group
@@ -32,6 +44,6 @@
 	})
 </script>
 
-<div id={group} use:forwardEvents {...$$restProps} class={classes}>
+<El {...props}>
 	<slot />
-</div>
+</El>
