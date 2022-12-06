9dc43332bfac0563a781b693a6eff6814b2ab7a1
Amir Pournasserian <amir.pournasserian@live.com>
2022-12-06 17:27:45 -0500

    #619 Page component refactoring and adding El

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -1,19 +1,44 @@
-<script>
-	import { Page, App, AppHeader, AppFooter, PageBody, Card, CardBody, El, AppBody } from '@ubeac/svelte'
+<script type="ts">
+	import { App, AppBody, AppFooter, AppHeader, Card, CardBody, El, Page, PageBody, PageHeader } from '@ubeac/svelte'
+
+	let container: ContainerMaxWidths = 'md'
+
+	let cssPrefixPreTitle = `page-header-pretitle`
+	let cssPrefixTitle = `page-header-title`
 </script>
 
 <App theme="light">
 	<AppHeader theme="dark">
-		<El container="md">
+		<El {container}>
 			<El row>
 				<El col>this is header</El>
 			</El>
 		</El>
 	</AppHeader>
 	<AppBody>
-		<Page>
+		<Page {container}>
+			{#if $$slots.preTitle || $$slots.preTitle}
+				<PageHeader>
+					<El {container}>
+						<El row>
+							<El col>
+								{#if $$slots.preTitle}
+									<slot name="preTitle">
+										<El cssPrefix={cssPrefixPreTitle}><slot /></El>
+									</slot>
+								{/if}
+								{#if $$slots.title}
+									<slot name="title">
+										<El tag="h2" cssPrefix={cssPrefixTitle}><slot /></El>
+									</slot>
+								{/if}
+							</El>
+						</El>
+					</El>
+				</PageHeader>
+			{/if}
 			<PageBody>
-				<El container="md">
+				<El {container}>
 					<El row>
 						<El col><Card><CardBody><slot /></CardBody></Card></El>
 					</El>
@@ -22,7 +47,7 @@
 		</Page>
 	</AppBody>
 	<AppFooter>
-		<El container="md">
+		<El {container}>
 			<El row>
 				<El col>this is footer</El>
 			</El>
