fdc5feacdb865baac34d93b1737dfbc744494d45
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-12-08 23:08:59 +0430

    add more examples for Popover

---
+++ src/routes/docs/components/popover/PopoverComponents.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { Button, FormInput, Popover, PopoverBody, FormRadioGroup, El } from '@ubeac/svelte'
+</script>
+
+<Button>Choose Filters</Button>
+
+<Popover trigger="click">
+	<!-- <PopoverHeader>Popover title</PopoverHeader> -->
+	<PopoverBody>
+		<El row>
+			<FormRadioGroup items={['equal', 'like', 'between']} label="Type" />
+			<FormInput label="Text" />
+		</El>
+	</PopoverBody>
+</Popover>
