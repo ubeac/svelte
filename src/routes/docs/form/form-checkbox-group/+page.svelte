781abd0d9a43db049f0a1147bab67528bb91cc79
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-12-15 22:09:50 +0430

    update examples of FormCheckboxGroup

--- src/routes/docs/form/form-checkbox-group/+page.svelte
+++ src/routes/docs/form/form-checkbox-group/+page.svelte
@@ -1,14 +1,47 @@
 <script>
-	import { Preview } from '@ubeac/svelte'
-	import FormCheckboxGroupDefault from './FormCheckboxGroupDefault.svelte'
+	import { Doc, Preview } from '@ubeac/svelte'
+	import FormCheckboxGroupBasic from './FormCheckboxGroupBasic.svelte'
+	import FormCheckboxGroupLabel from './FormCheckboxGroupLabel.svelte'
+	import FormCheckboxGroupColor from './FormCheckboxGroupColor.svelte'
+	import FormCheckboxGroupArrayBinding from './FormCheckboxGroupArrayBinding.svelte'
+	import FormCheckboxGroupInline from './FormCheckboxGroupInline.svelte'
 </script>
 
 <h1>FormCheckboxGroup</h1>
-<p>FormCheckboxGroup is similar to <a href="">CheckboxGroup</a> component with some additional features</p>
 
-<h2>Label</h2>
-<p>FormCheckboxGroup can have label.</p>
+<Doc title="Basic usage">
+	FormCheckboxGroup is similar to <a href="">CheckboxGroup</a> component with some additional features.
+</Doc>
+<Preview src="./FormCheckboxGroupBasic.svelte">
+	<FormCheckboxGroupBasic />
+</Preview>
 
-<Preview src="./FormCheckboxGroupDefault.svelte">
-	<FormCheckboxGroupDefault />
+<Doc title="Custom label">
+	<!--  -->
+</Doc>
+<Preview src="./FormCheckboxGroupLabel.svelte">
+	<FormCheckboxGroupLabel />
+</Preview>
+
+<Doc title="Colors">
+	Set <code>color</code> property to define background color for all <code>Checkboxes</code> inside the
+	<code>CheckboxGroup</code>
+</Doc>
+<Preview src="./FormCheckboxGroupColor.svelte">
+	<FormCheckboxGroupColor />
+</Preview>
+
+<Doc title="Inline">
+	Set <code>inline</code> property to display inline all <code>Checkboxes</code> inside the
+	<code>CheckboxGroup</code>
+</Doc>
+<Preview src="./FormCheckboxGroupInline.svelte">
+	<FormCheckboxGroupInline />
+</Preview>
+
+<Doc title="Binding">
+	Set an array of objects to <code>items</code> property and define <code>label</code>.
+</Doc>
+<Preview src="./FormCheckboxGroupArrayBinding.svelte">
+	<FormCheckboxGroupArrayBinding />
 </Preview>
---
+++ src/routes/docs/form/form-checkbox-group/FormCheckboxGroupColor.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { FormCheckboxGroup } from '@ubeac/svelte'
+
+	// define array to be bound to the CheckboxGroup
+	let items = ['HTML', 'CSS', 'JavaScript', 'Svelte']
+
+	// setting the selected value
+	let value = [items[1]]
+</script>
+
+<FormCheckboxGroup label="Select Language" inline color="primary" {items} bind:value />
+<FormCheckboxGroup label="Select Language" inline color="secondary" {items} bind:value />
+<FormCheckboxGroup label="Select Language" inline color="success" {items} bind:value />
+<FormCheckboxGroup label="Select Language" inline color="danger" {items} bind:value />
+
+<i>value: {value}</i>
