75cfe7220d7de300c0fe9fd2bc4eccff97b9175f
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-12-16 11:01:24 +0430

    cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,3 @@
-<script>
-	import { Checkbox, FormCheckbox, CheckboxGroup, FormCheckboxGroup } from '../components'
-	import FormRadioGroup from '../components/Form/FormRadioGroup.svelte'
-
-	let value
-	let group = ['html', 'css']
-	let radioValue
-
-	$: console.log({ value })
-	$: console.log({ group })
-</script>
-
-wer
 <a href="/docs">Documentation website</a>
 <br />
 <a href="https://svelte.ubeac.io/storybook">Storybook</a>
-
-<Checkbox bind:value>Test</Checkbox>
-<FormCheckbox bind:value>Test</FormCheckbox>
-
-<CheckboxGroup bind:value={group} items={['html', 'css', 'js', 'svelte']} />
-
-<FormCheckboxGroup label="TEST" bind:value={group} items={['html', 'css', 'js', 'svelte']} />
-<FormRadioGroup bind:value={radioValue} items={['html', 'css', 'js', 'svelte']} />
-
-{radioValue}
