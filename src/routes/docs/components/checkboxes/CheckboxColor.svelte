3d2554189b0ad5a181944690087f433b5ae4e1fb
Amir Pournasserian <amir.pournasserian@live.com>
2022-12-16 15:59:07 -0500

    #661 Checkbox revision

--- src/routes/docs/components/checkboxes/CheckboxColor.svelte
+++ src/routes/docs/components/checkboxes/CheckboxColor.svelte
@@ -1,10 +1,9 @@
 <script>
 	import { Checkbox } from '@ubeac/svelte'
-	import { COLORS } from '@ubeac/svelte/types'
-
-	let checked = true
 </script>
 
-{#each COLORS as color}
-	<Checkbox bind:value={checked} label={color} {color} />
-{/each}
+<Checkbox color="primary" label="Primary" value={true} />
+<Checkbox color="secondary" label="Secondary" value={true} />
+<Checkbox color="success" label="Success" value={true} />
+<Checkbox color="warning" label="Warning" value={true} />
+<Checkbox color="danger" label="Danger" value={true} />
--- src/routes/docs/components/checkboxes/CheckboxDefault.svelte
+++ src/routes/docs/components/checkboxes/CheckboxDefault.svelte
@@ -3,7 +3,10 @@
 
 	let value1 = false
 	let value2 = true
+	let value3 = false
 </script>
 
-<Checkbox bind:value={value1} label="checkbox 1" />
-<Checkbox bind:value={value2} label="checkbox 2" />
+<Checkbox bind:value={value1} label="Option 1" />
+<Checkbox bind:value={value2} label="Option 2" />
+<Checkbox bind:value={value3} label="Option 3" />
+<i>Options: {value1}, {value2}, {value3} </i>
