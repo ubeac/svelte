0c20a94eccffbf1a287a3043821d2ea1f86863e8
Amir Pournasserian <amir.pournasserian@live.com>
2022-12-22 17:18:40 -0500

    Update ColorsNamed.svelte
    Removing unused example

--- src/routes/docs/core/contents/colors/ColorsNamed.svelte
+++ src/routes/docs/core/contents/colors/ColorsNamed.svelte
@@ -14,16 +14,3 @@
 <El bgColor="green">Green</El>
 <El bgColor="teal">Teal</El>
 <El bgColor="cyan">Cyan</El>
-
-<El bgColor="blue-lt">Blue</El>
-<El bgColor="azure-lt">Azure</El>
-<El bgColor="indigo-lt">Indigo</El>
-<El bgColor="purple-lt">Purple</El>
-<El bgColor="pink-lt">Pink</El>
-<El bgColor="red-lt">Red</El>
-<El bgColor="orange-lt">Orange</El>
-<El bgColor="yellow-lt">Yellow</El>
-<El bgColor="lime-lt">Lime</El>
-<El bgColor="green-lt">Green</El>
-<El bgColor="teal-lt">Teal</El>
-<El bgColor="cyan-lt">Cyan</El>
