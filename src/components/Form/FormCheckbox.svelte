3ddcd2622c7e5b1d6e8b309c999fc8ea2908dbb6
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-12-25 21:37:07 +0430

    #716 - add required prop to FormCheckbox

--- src/components/Form/FormCheckbox.svelte
+++ src/components/Form/FormCheckbox.svelte
@@ -5,11 +5,21 @@
 	type $$Props = FormCheckboxProps
 
 	export let color: $$Props['color'] = undefined
-	export let cssPrefix: $$Props['cssPrefix'] = 'form-checkbox'
+	export let label: $$Props['label'] = undefined
+	export let hint: $$Props['hint'] = undefined
+	export let state: $$Props['state'] = undefined
 	export let inline: $$Props['inline'] = undefined
 	export let description: $$Props['description'] = undefined
+	export let required: $$Props['required'] = undefined
 	export let reverse: $$Props['reverse'] = undefined
 	export let value: $$Props['value'] = undefined
+
+	$: props = {
+		required,
+		label,
+		hint,
+		state,
+	}
 
 	$: checkboxProps = {
 		color,
@@ -19,7 +29,7 @@
 	}
 </script>
 
-<FormField {...$$restProps} {cssPrefix}>
+<FormField {...props} {...$$restProps}>
 	<slot name="label" />
 	<Checkbox {...checkboxProps} bind:value>
 		<slot />
