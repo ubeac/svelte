dc35ec56c44e2c4755018c404338a1e0e5ba6340
Amir Pournasserian <ap@momentaj.com>
2022-12-25 10:36:23 -0500

    Divider cleanup

--- src/routes/docs/components/dividers/DividerDirection.svelte
+++ src/routes/docs/components/dividers/DividerDirection.svelte
@@ -2,15 +2,6 @@
 	import { Divider, El } from '@ubeac/svelte'
 </script>
 
-<El display="flex" style="height: 200px">
-	<El h="100" w="75" p="4" textColor="primary">One</El>
-	<Divider direction="vertical" />
-	<El h="100" w="75" p="4" textColor="secondary">Two</El>
-	<Divider direction="vertical" />
-	<El h="100" w="75" p="4" textColor="success">Three</El>
-</El>
-
-<br />
 <El display="flex" gap="2">
 	<El textColor="primary">Primary</El>
 	<Divider direction="vertical" />
