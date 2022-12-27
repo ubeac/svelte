6ddd75736d5af695e63b19e10635328b4b23c14d
Mohammad Taherian <mhmdtaherian@gmail.com>
2022-12-27 08:56:00 -0500

    started component

---
+++ src/components/DropdownButton/DropdownButton.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { type DropdownButtonProps, El } from '$lib/components'
+
+	type $$Props = DropdownButtonProps
+
+	/**
+	 * Set Css Prefix for the Divider
+	 */
+	export let cssPrefix: $$Props['cssPrefix'] = 'dropdown-button'
+
+	/**
+	 * Set color of Divider
+	 */
+	export let color: $$Props['color'] = undefined
+
+	/**
+	 * Set color of Divider
+	 */
+	export let items: $$Props['items'] = undefined
+
+	// /**
+	//  * Set alignment of Divider
+	//  */
+	// export let alignment: $$Props['alignment'] = undefined
+
+	let cssProps: any = {}
+	let otherProps: any = {}
+	$: {
+		cssProps = {
+			//alignment,
+			color,
+		}
+		otherProps = {
+			cssPrefix,
+		}
+	}
+</script>
+
+<El {...$$restProps} {cssProps} {...otherProps}>
+	<slot />
+	<El tag="ul" cssPrefix={cssPrefix + '-items'}>
+		{#each items as item}
+			<El tag="li" cssPrefix={cssPrefix + '-item'}>{item}</El>
+		{/each}
+	</El>
+</El>
---
+++ src/components/DropdownButton/index.ts
@@ -0,0 +1,2 @@
+export { default as DropdownButton } from './DropdownButton.svelte'
+export * from './DropdownButton.types'
