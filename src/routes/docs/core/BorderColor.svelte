b7e27f469ac3ec9a4c170f20fe0ac0606a47e701
Amir Pournasserian <amir.pournasserian@live.com>
2022-12-28 18:16:45 -0500

    #774 documentation tyle enhancement in El>border
    close #774

--- src/routes/docs/core/BorderColor.svelte
+++ src/routes/docs/core/BorderColor.svelte
@@ -2,11 +2,11 @@
 	import { El } from '@ubeac/svelte'
 </script>
 
-<El border borderColor="primary">primary</El>
+<El border borderColor="primary">Primary</El>
 <El border borderColor="secondary">Secondary</El>
 <El border borderColor="success">Success</El>
 <El border borderColor="danger">Danger</El>
 <El border borderColor="warning">Warning</El>
+<El border borderColor="info">Info</El>
 <El border borderColor="light">Light</El>
 <El border borderColor="dark">Dark</El>
-<El border borderColor="white">White</El>
