acd3ed42640177dcba9d80105660ed4e1bcfb595
Mohammad Taherian <mhmdtaherian@gmail.com>
2023-01-06 13:26:39 -0500

    fixed padding issue in cards

--- src/routes/(docs)/docs/components/cards/CardFooter.svelte
+++ src/routes/(docs)/docs/components/cards/CardFooter.svelte
@@ -3,51 +3,61 @@
 </script>
 
 <El row>
-	<Card col="12" title="Card with footer">
-		<CardBody
-			>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-			standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
-			type specimen book.</CardBody>
-		<CardFooter>This is standard card footer</CardFooter>
-	</Card>
-	<Card col="12" title="Card with transparent footer">
-		<CardBody
-			>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-			standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
-			type specimen book.</CardBody>
-		<CardFooter transparent>This is transparent card footer</CardFooter>
-	</Card>
-	<Card col="12" title="Card with footer button">
-		<CardBody
-			>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-			standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
-			type specimen book.</CardBody>
-		<CardFooter><Button color="primary">Go somewhere</Button></CardFooter>
-	</Card>
-	<Card col="12" title="Card with footer buttons">
-		<CardBody
-			>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-			standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
-			type specimen book.</CardBody>
-		<CardFooter>
-			<El d="flex" w="100">
-				<Button color="secondary">Cancel</Button>
-				<Button color="primary" ms="auto">Go somewhere</Button>
-			</El>
-		</CardFooter>
-	</Card>
+	<El col="12">
+		<Card title="Card with footer">
+			<CardBody
+				>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+				standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+				a type specimen book.</CardBody>
+			<CardFooter>This is standard card footer</CardFooter>
+		</Card>
+	</El>
+	<El col="12">
+		<Card title="Card with transparent footer">
+			<CardBody
+				>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+				standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+				a type specimen book.</CardBody>
+			<CardFooter transparent>This is transparent card footer</CardFooter>
+		</Card>
+	</El>
+	<El col="12">
+		<Card title="Card with footer button">
+			<CardBody
+				>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+				standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+				a type specimen book.</CardBody>
+			<CardFooter><Button color="primary">Go somewhere</Button></CardFooter>
+		</Card>
+	</El>
+	<El col="12">
+		<Card title="Card with footer buttons">
+			<CardBody
+				>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+				standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+				a type specimen book.</CardBody>
+			<CardFooter>
+				<El d="flex" w="100">
+					<Button color="secondary">Cancel</Button>
+					<Button color="primary" ms="auto">Go somewhere</Button>
+				</El>
+			</CardFooter>
+		</Card>
+	</El>
 </El>
 <El row>
-	<Card col title="Card with action footer">
-		<CardBody
-			>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-			standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a
-			type specimen book.</CardBody>
-		<CardFooter>
-			<CardActions>
-				<Button color="secondary">Cancel</Button>
-				<Button color="primary" ms="auto">Go somewhere</Button>
-			</CardActions>
-		</CardFooter>
-	</Card>
+	<El col="12">
+		<Card title="Card with action footer">
+			<CardBody
+				>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+				standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
+				a type specimen book.</CardBody>
+			<CardFooter>
+				<CardActions>
+					<Button color="secondary">Cancel</Button>
+					<Button color="primary" ms="auto">Go somewhere</Button>
+				</CardActions>
+			</CardFooter>
+		</Card>
+	</El>
 </El>
--- src/routes/(docs)/docs/components/cards/CardTitle.svelte
+++ src/routes/(docs)/docs/components/cards/CardTitle.svelte
@@ -3,25 +3,35 @@
 </script>
 
 <El row>
-	<Card title="Card title" col="12">
-		<CardBody>This is some text within a card body.</CardBody>
-	</Card>
-	<Card col="12">
-		<CardHeader>
-			<CardTitle>Card title in card header</CardTitle>
-		</CardHeader>
-		<CardBody>This is some text within a card body.</CardBody>
-	</Card>
-	<Card borderless col="12">
-		<CardHeader>
-			<CardTitle>Borderless card</CardTitle>
-		</CardHeader>
-		<CardBody>This is some text within a card body.</CardBody>
-	</Card>
-	<Card>
-		<CardHeader light col="12">
-			<CardTitle>Card title in card header</CardTitle>
-		</CardHeader>
-		<CardBody>This is some text within a card body.</CardBody>
-	</Card>
+	<El col="12">
+		<Card title="Card title">
+			<CardBody>This is some text within a card body.</CardBody>
+		</Card>
+	</El>
+	<El col="12">
+		<Card>
+			<CardHeader>
+				<CardTitle>Card title in card header</CardTitle>
+			</CardHeader>
+			<CardBody>This is some text within a card body.</CardBody>
+		</Card>
+	</El>
+
+	<El col="12">
+		<Card borderless>
+			<CardHeader>
+				<CardTitle>Borderless card</CardTitle>
+			</CardHeader>
+			<CardBody>This is some text within a card body.</CardBody>
+		</Card>
+	</El>
+
+	<El col="12">
+		<Card>
+			<CardHeader light>
+				<CardTitle>Card title in card header</CardTitle>
+			</CardHeader>
+			<CardBody>This is some text within a card body.</CardBody>
+		</Card>
+	</El>
 </El>
