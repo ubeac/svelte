c3addbde397fbff45d14c8f92fd2adc2ed29138c
Amir Pournasserian <amir.pournasserian@live.com>
2023-01-06 13:05:26 -0500

    Removing unused test routes

--- src/routes/test-direct-el/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-	import El from '@ubeac/svelte/components/El/El.svelte'
-</script>
-
-<El tag="h1">Hello World!</El>
--- src/routes/test-el/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-	import {El} from '@ubeac/svelte'
-</script>
-
-<El tag="h1">Hello World!</El>
