882d20c37202ae90271b18ddf9a53d08918770ec
Amir Pournasserian <amir.pournasserian@live.com>
2023-01-16 11:09:49 -0500

    #872 removing unused requestAnimationFrame.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,1 @@
 export * from './classname'
-export * from './requestAnimationFrame'
--- src/utils/requestAnimationFrame.ts
+++
@@ -1,4 +0,0 @@
-export const requestAnimationFrame = (callback: any) => {
-	if (typeof window == 'undefined') return
-	window.requestAnimationFrame(callback)
-}
