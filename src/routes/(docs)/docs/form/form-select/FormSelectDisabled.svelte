0e61c844f2411e7474f00d9bd9fb3770f7380123
Masood Abdolian <m.abdolian@gmail.com>
2023-01-20 01:09:09 +0330

    updated form select documentation

---
+++ src/routes/(docs)/docs/form/form-select/FormSelectDisabled.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { FormSelect } from '@ubeac/svelte'
+
+	let items = ['Apple', 'Orange', 'Peach', 'Banana', 'Apricot']
+	let value: any = undefined
+</script>
+
+<FormSelect label="Label" disabled bind:value {items} />
---
+++ src/routes/(docs)/docs/form/form-select/FormSelectPlaceholder.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { FormSelect } from '@ubeac/svelte'
+
+	let items = ['Apple', 'Orange', 'Peach', 'Banana', 'Apricot']
+	let value: any = undefined
+</script>
+
+<FormSelect label="Label" bind:value {items} placeholder="Please select an item" />
+<i>Selected value: {value}</i>
