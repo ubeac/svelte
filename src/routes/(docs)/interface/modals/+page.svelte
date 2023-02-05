15965b21487a4ddce3318c57a673497159f7b80c
smile-armin <armin@smileitsolutions.uk>
2023-02-05 13:03:29 +0330

    fix dialog

--- src/routes/(docs)/interface/modals/+page.svelte
+++ src/routes/(docs)/interface/modals/+page.svelte
@@ -1,37 +1,36 @@
 <script lang="ts">
-    import {Card, CardBody, El, Button} from '@ubeac/svelte'
-    import SimpleModal from "./SimpleModal.svelte";
-    import LargeModal from "./LargeModal.svelte";
-    import SmallModal from "./SmallModal.svelte";
-    import FullWidthModal from "./FullWidthModal.svelte";
-    import ScrollableModal from "./ScrollableModal.svelte";
-    import FormModal from "./FormModal.svelte";
-    import SimpleFormModal from "./SimpleFormModal.svelte";
+	import { Button, Card, CardBody, El } from '@ubeac/svelte'
 
-    let simpleModalIsOpen = false;
+	import DangerModal from './DangerModal.svelte'
+	import FormModal from './FormModal.svelte'
+	import FullWidthModal from './FullWidthModal.svelte'
+	import LargeModal from './LargeModal.svelte'
+	import ScrollableModal from './ScrollableModal.svelte'
+	import SimpleFormModal from './SimpleFormModal.svelte'
+	import SimpleModal from './SimpleModal.svelte'
+	import SmallModal from './SmallModal.svelte'
+	import SuccessModal from './SuccessModal.svelte'
+
+	let simpleModalIsOpen = false
 </script>
-<h1>Modals</h1>
-<El lg>
-  <El row>
-    <El col="12" d="none" dMd="block">
-      <Card size="lg">
-        <CardBody class="px-4 py-3">
-          <SimpleModal/>
-          <LargeModal/>
-          <SmallModal/>
-          <FullWidthModal/>
-          <ScrollableModal/>
-          <FormModal/>
-          <Button>
-            Success modal
-          </Button>
-          <Button class="m-2">
-            Danger modal
-          </Button>
-          <SimpleFormModal/>
-        </CardBody>
-      </Card>
-    </El>
-  </El>
-</El>
 
+<h1>Modals</h1>
+<El>
+	<El row>
+		<El col="12" d="none" dMd="block">
+			<Card size="lg">
+				<CardBody class="px-4 py-3">
+					<SimpleModal />
+					<LargeModal />
+					<SmallModal />
+					<FullWidthModal />
+					<ScrollableModal />
+					<FormModal />
+					<SuccessModal />
+					<DangerModal />
+					<SimpleFormModal />
+				</CardBody>
+			</Card>
+		</El>
+	</El>
+</El>
---
+++ src/routes/(docs)/interface/modals/SuccessModal.svelte
@@ -0,0 +1,41 @@
+<script>
+	import {
+		Button,
+		Dialog,
+		DialogBody,
+		DialogClose,
+		DialogContent,
+		DialogFooter,
+		DialogHeader,
+		DialogTitle,
+		Card,
+		CardBody,
+		CardTitle,
+	} from '@ubeac/svelte'
+	import Icon from '@ubeac/svelte/components/Icon/Icon.svelte'
+
+	let open = false
+</script>
+
+<Button on:click={() => (open = true)}>Success modal</Button>
+
+<Dialog size="sm" bind:open>
+	<DialogContent>
+		<Card class="flex" justifyContent="center" status statusColor="success" statusPosition="top" statusSize="lg">
+			<CardTitle>
+				<DialogClose />
+			</CardTitle>
+			<CardBody class="text-center" alignItems="center">
+				<Icon size="6x" color="success" name="circle-check" />
+				<h3>Payment succedeed</h3>
+				<p class="text-center">
+					Your payment of $290 has been successfully submitted. Your invoice has been sent to support@tabler.io.
+				</p>
+				<div>
+					<Button class="me-auto" on:click={() => (open = false)}>Close</Button>
+					<Button color="success" on:click={() => (open = false)}>View invoice</Button>
+				</div>
+			</CardBody>
+		</Card>
+	</DialogContent>
+</Dialog>
