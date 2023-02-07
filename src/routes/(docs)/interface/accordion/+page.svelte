aca4172422ad3d4bf8323dc4beb3e52eea04d7d5
Amir Pournasserian <ap@momentaj.com>
2023-02-07 11:53:27 -0500

    Accordion interface cleanup

--- src/routes/(docs)/interface/accordion/+page.svelte
+++ src/routes/(docs)/interface/accordion/+page.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
-  import { Card, CardBody, Accordions, Accordion, AccordionBody } from '@ubeac/svelte'
+	import { Accordion, AccordionBody, Accordions, Card, CardBody } from '@ubeac/svelte'
 
-  const description = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur qui facere, explicabo hic eius veritatis\n" +
-    "commodi quasi dignissimos quia odit obcaecati consectetur? Officiis nam earum tenetur unde, cum beatae minus?";
-  const title = "Accordion Item #";
+	const description =
+		'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur qui facere, explicabo hic eius veritatis\n' +
+		'commodi quasi dignissimos quia odit obcaecati consectetur? Officiis nam earum tenetur unde, cum beatae minus?'
+	const title = 'Accordion Item #'
 </script>
+
 <h1>Accordion</h1>
-<Card size="lg">
-  <CardBody class="px-4 py-3">
-    <Accordions>
-      {#each Array(5) as _, index}
-        <Accordion title="{title}{index +1}">
-          <AccordionBody>{description}</AccordionBody>
-        </Accordion>
-      {/each}
-    </Accordions>
-  </CardBody>
+<Card>
+	<CardBody>
+		<Accordions>
+			{#each Array(5) as _, index}
+				<Accordion title="{title}{index + 1}">
+					<AccordionBody>{description}</AccordionBody>
+				</Accordion>
+			{/each}
+		</Accordions>
+	</CardBody>
 </Card>
