609eab4a6727deb6f9925503b15951da3d411b29
Amir Pournasserian <amir.pournasserian@live.com>
2023-02-08 13:26:14 -0500

    Update +layout.svelte

--- src/routes/(docs)/+layout.svelte
+++ src/routes/(docs)/+layout.svelte
@@ -478,41 +478,6 @@
 				<div class="navbar navbar-light">
 					<div class="container-xl">
 						<ul class="navbar-nav">
-							<li class="nav-item dropdown">
-								<a
-									class="nav-link dropdown-toggle"
-									href="#navbar-base"
-									data-bs-toggle="dropdown"
-									data-bs-auto-close="outside"
-									role="button"
-									aria-expanded="false">
-									<span class="nav-link-icon d-md-none d-lg-inline-block"
-										><!-- Download SVG icon from http://tabler-icons.io/i/package -->
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											class="icon"
-											width="24"
-											height="24"
-											viewBox="0 0 24 24"
-											stroke-width="2"
-											stroke="currentColor"
-											fill="none"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											><path stroke="none" d="M0 0h24v24H0z" fill="none" /><polyline
-												points="12 3 20 7.5 20 16.5 12 21 4 16.5 4 7.5 12 3" /><line
-												x1="12"
-												y1="12"
-												x2="20"
-												y2="7.5" /><line x1="12" y1="12" x2="12" y2="21" /><line x1="12" y1="12" x2="4" y2="7.5" /><line
-												x1="16"
-												y1="5.25"
-												x2="8"
-												y2="9.75" /></svg>
-									</span>
-									<span class="nav-link-title"> Interface </span>
-								</a>
-							</li>
 							<li class="nav-item">
 								<a class="nav-link" href="https://tabler-icons.io/" target="_blank" rel="noreferrer">
 									<span class="nav-link-icon d-md-none d-lg-inline-block"
