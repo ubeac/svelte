b34848bd9dcf18885712ff603908628f9873ea46
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2023-02-21 19:45:38 +0300

    add a postcss/tailwind setup to generate Tailwind utility classes if needed

---
+++ src/scss/daisyui/tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+	content: ['./src/scss/daisyui/components/**/*.scss'],
+	// content: {
+	// 	layers: ['base', 'utilities'],
+	// },
+	plugins: [],
+	safelist: [
+		// 'btn',
+		// {
+		// 	pattern: /bg-(red|green|blue)-(100|200|300)/,
+		// },
+	],
+}
